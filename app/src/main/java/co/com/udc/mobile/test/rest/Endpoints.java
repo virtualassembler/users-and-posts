35e51cefc55f6457e4486d24abec23662cbd7f49
david.mazo <david.mazo@globant.com>
2019-12-19 12:50:10 -0500

    filter layout added

--- app/src/main/java/co/com/udc/mobile/test/rest/Endpoints.java
+++ app/src/main/java/co/com/udc/mobile/test/rest/Endpoints.java
@@ -4,4 +4,5 @@ public class Endpoints {
     public static final String URL_BASE = "https://jsonplaceholder.typicode.com";
     public static final String GET_USERS = "/users";
     public static final String GET_POST_USER = "/posts?";
+    public static final String GET_POSTS_BY_USER_ID = "/posts?userId=";
 }
